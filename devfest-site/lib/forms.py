d353b13c289ece2b51922163b6846bddf98844ea
Martin Thielecke <martin@versionzwei.com>
2012-08-06 19:22:56 +0000

    changed form from model-form to an individual one

--- devfest-site/lib/forms.py
+++ devfest-site/lib/forms.py
@@ -1,5 +1,13 @@
-from wtforms import Form
+from wtforms import Form, TextField, SelectField, SelectMultipleField, widgets, validators
 from wtforms.ext.appengine.db import model_form
 from lib.model import Event
 
-EventForm = model_form(Event, Form)
+class MultiCheckboxField(SelectMultipleField):
+  widget = widgets.ListWidget(prefix_label=False)
+  option_widget = widgets.CheckboxInput()
+
+class EventForm(Form):
+  gplus_event_url = TextField('Google+ Event URL', [validators.URL(), validators.Required()])
+  location = TextField('Location', [validators.Length(min=3), validators.Required()])
+  status = SelectField('Status', choices=[('1', 'interested'), ('2', 'planned'), ('3', 'confirmed')])
+  agenda = MultiCheckboxField('Agenda', choices=[('1', 'Conference'), ('2', 'Hackathon/VHackAndroid'), ('3', 'Barcamp'), ('4', 'Google Developer Live sessions')])
