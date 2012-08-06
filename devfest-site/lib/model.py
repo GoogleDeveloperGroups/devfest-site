4b14123882bdf53859a139a9b9e4e0da7218d013
Martin Thielecke <martin@versionzwei.com>
2012-08-06 19:23:25 +0000

    removed the choices

--- devfest-site/lib/model.py
+++ devfest-site/lib/model.py
@@ -4,17 +4,17 @@ class Event(db.Model):
   gplus_id = db.StringProperty()
   gplus_img_url = db.StringProperty()
   gplus_event_url = db.StringProperty()
-  status = db.StringProperty(choices=set(["interested", "planned", "confirmed"]))    
-  location = db.StringProperty(required=True)
+  status = db.StringProperty()
+  location = db.StringProperty()
   city = db.StringProperty()
   country = db.StringProperty()
   geo_location = db.GeoPtProperty()
-  agenda = db.StringListProperty(choices=set(["Conference", "Hackathon/VHackAndroid", "Barcamp", "Google Developer Live sessions"]))
+  agenda = db.StringListProperty()
   agenda_description = db.StringProperty()
     
 class UserGroup(db.Model):
   gplus_id = db.StringProperty(required=True)
-  group_name = db.StringProperty()   
+  group_name = db.StringProperty()
   gplus_img_url = db.StringProperty()
   event = db.ReferenceProperty(Event)
 
