dd250c34d6bfa783c4e2c97e1bfc92b5b8d01a42
Martin Thielecke <martin@versionzwei.com>
2012-08-07 20:53:35 +0000

    - removed needless session config

--- devfest-site/admin.py
+++ devfest-site/admin.py
@@ -1,14 +1,8 @@
 import webapp2
 from pages.AdminPage import *
 
-config = {}
-config['webapp2_extras.sessions'] = {
-       'secret_key': 'C^c)2!]%Y+-9S)@[x)W/;6uYh=;}oz',
-      }
-
 app = webapp2.WSGIApplication([
                               ('^/admin/get_access_token$', AdminAuthTokenPage),
                               ('^/admin$', AdminPage),
                               ],
-                              debug=True, config=config)
-
+                              debug=True)
