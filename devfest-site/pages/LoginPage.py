40d8c2b0745d0f6fc12ede69386d7c462e37d5ba
Martin Thielecke <martin@versionzwei.com>
2012-08-07 20:55:57 +0000

    - added logout page

--- devfest-site/pages/LoginPage.py
+++ devfest-site/pages/LoginPage.py
@@ -1,6 +1,11 @@
 from lib.view import FrontendPage
 from google.appengine.api import users
 
+class LogoutPage(FrontendPage):
+  def show(self):
+    self.template = 'login'
+    return self.redirect(users.create_logout_url("/"))
+
 class LoginPage(FrontendPage):
   def show(self):
     self.template = 'login'
