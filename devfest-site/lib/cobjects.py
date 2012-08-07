25d862b70712df8b7c72fe50a55a5d4a6aba6e22
Martin Thielecke <martin@versionzwei.com>
2012-08-07 20:54:20 +0000

    - get the event and eventlist from google docs instead of the database

--- devfest-site/lib/cobjects.py
+++ devfest-site/lib/cobjects.py
@@ -1,3 +1,10 @@
+try:
+  import settings_local as settings
+except:
+  import settings
+
+import gdata.gauth
+import gdata.spreadsheets.client
 from django.core.serializers import deserialize, serialize
 from google.appengine.api import memcache
 from google.appengine.ext import db, search
@@ -66,16 +73,19 @@ class CEventList(OCachedObject):
 
   def load_from_db(self):
     self.entity_collection = {}
+    client = gdata.spreadsheets.client.SpreadsheetsClient(source='Devfest-Website-v1')
+    access_token = gdata.gauth.AeLoad('spreadsheed_token')
+    feed = client.get_list_feed(settings.DOCSAPI_SPREADSHEET_ID,
+                                settings.DOCSAPI_SPREADSHEET_WORKSHEET_ID,
+                                auth_token=access_token)
+    if feed.entry:
+      for entry in feed.entry:
+        event = entry.to_dict()
+        if event['country'] not in self.entity_collection:
+          self.entity_collection[event['country']] = []
 
-    events = Event.all()
-    event_list = {}
-    for event in events:
-      if event_list.has_key(event.country) is False:
-        event_list[event.country] = []
-
-      event_list[event.country].append(event)
-
-    self.entity_collection = event_list
+        event['id'] = entry.link[0].href.split('/')[-1]
+        self.entity_collection[event['country']].append(event)
 
   def get(self):
     return self.entity_collection
@@ -86,7 +96,7 @@ class CEventList(OCachedObject):
     return_value = {}
     i = 0
     for key in self.entity_collection:
-      if i < half_length:
+      if i <= half_length:
         return_value[key] = self.entity_collection[key]
       i = i+1
 
@@ -98,25 +108,36 @@ class CEventList(OCachedObject):
     return_value = {}
     i = 0
     for key in self.entity_collection:
-      if i >= half_length:
+      if i > half_length:
         return_value[key] = self.entity_collection[key]
       i = i+1
 
     return return_value
 
-class CEvent(CachedObject):
+class CEvent(OCachedObject):
   def __init__(self, event_id):
     self.event_id = event_id
     self.max_time = 3600
-    self.cache_key = "Event(%s)" % (self.id)
+    self.cache_key = "Event(%s)" % (self.event_id)
     CachedObject.__init__(self)
 
   def load_from_db(self):
     self.entity_collection = None
-    data = Event.get_by_id(id)
+    client = gdata.spreadsheets.client.SpreadsheetsClient(source='Devfest-Website-v1')
+    access_token = gdata.gauth.AeLoad('spreadsheed_token')
+    feed = client.get_list_feed(settings.DOCSAPI_SPREADSHEET_ID,
+                                settings.DOCSAPI_SPREADSHEET_WORKSHEET_ID,
+                                auth_token=access_token)
 
-    if isinstance(data, Event):
-      self.entity_collection = data
+    event = None
+    if feed.entry:
+      for entry in feed.entry:
+        event_id = entry.link[0].href.split('/')[-1]
+        if event_id == self.event_id:
+          event = entry.to_dict()
+          event['id'] = self.event_id
+
+    self.entity_collection = event
 
   def get(self):
     return self.entity_collection
