16b34110e3d169110b86128617df149c2c69a960
Martin Thielecke <martin@versionzwei.com>
2012-08-10 20:41:03 +0000

    - let's use the database again

--- devfest-site/lib/cobjects.py
+++ devfest-site/lib/cobjects.py
@@ -73,19 +73,30 @@ class CEventList(OCachedObject):
 
   def load_from_db(self):
     self.entity_collection = {}
-    client = gdata.spreadsheets.client.SpreadsheetsClient(source='Devfest-Website-v1')
-    access_token = gdata.gauth.AeLoad('spreadsheed_token')
-    feed = client.get_list_feed(settings.DOCSAPI_SPREADSHEET_ID,
-                                settings.DOCSAPI_SPREADSHEET_WORKSHEET_ID,
-                                auth_token=access_token)
-    if feed.entry:
-      for entry in feed.entry:
-        event = entry.to_dict()
-        if event['country'] not in self.entity_collection:
-          self.entity_collection[event['country']] = []
 
-        event['id'] = entry.link[0].href.split('/')[-1]
-        self.entity_collection[event['country']].append(event)
+    events = Event.all()
+    event_list = {}
+    for event in events:
+      if event_list.has_key(event.country) is False:
+        event_list[event.country] = []
+
+      event_list[event.country].append(event)
+
+    self.entity_collection = event_list
+#    self.entity_collection = {}
+#    client = gdata.spreadsheets.client.SpreadsheetsClient(source='Devfest-Website-v1')
+#    access_token = gdata.gauth.AeLoad('spreadsheed_token')
+#    feed = client.get_list_feed(settings.DOCSAPI_SPREADSHEET_ID,
+#                                settings.DOCSAPI_SPREADSHEET_WORKSHEET_ID,
+#                                auth_token=access_token)
+#    if feed.entry:
+#      for entry in feed.entry:
+#        event = entry.to_dict()
+#        if event['country'] not in self.entity_collection:
+#          self.entity_collection[event['country']] = []
+#
+#        event['id'] = entry.link[0].href.split('/')[-1]
+#        self.entity_collection[event['country']].append(event)
 
   def get(self):
     return self.entity_collection
@@ -114,7 +125,7 @@ class CEventList(OCachedObject):
 
     return return_value
 
-class CEvent(OCachedObject):
+class CEvent(CachedObject):
   def __init__(self, event_id):
     self.event_id = event_id
     self.max_time = 3600
@@ -123,21 +134,31 @@ class CEvent(OCachedObject):
 
   def load_from_db(self):
     self.entity_collection = None
-    client = gdata.spreadsheets.client.SpreadsheetsClient(source='Devfest-Website-v1')
-    access_token = gdata.gauth.AeLoad('spreadsheed_token')
-    feed = client.get_list_feed(settings.DOCSAPI_SPREADSHEET_ID,
-                                settings.DOCSAPI_SPREADSHEET_WORKSHEET_ID,
-                                auth_token=access_token)
 
-    event = None
-    if feed.entry:
-      for entry in feed.entry:
-        event_id = entry.link[0].href.split('/')[-1]
-        if event_id == self.event_id:
-          event = entry.to_dict()
-          event['id'] = self.event_id
+    try:
+      data = Event.get(self.event_id)
 
-    self.entity_collection = event
+      if isinstance(data, Event):
+        self.entity_collection = data
+    except:
+      pass
+
+#    self.entity_collection = None
+#    client = gdata.spreadsheets.client.SpreadsheetsClient(source='Devfest-Website-v1')
+#    access_token = gdata.gauth.AeLoad('spreadsheed_token')
+#    feed = client.get_list_feed(settings.DOCSAPI_SPREADSHEET_ID,
+#                                settings.DOCSAPI_SPREADSHEET_WORKSHEET_ID,
+#                                auth_token=access_token)
+#
+#    event = None
+#    if feed.entry:
+#      for entry in feed.entry:
+#        event_id = entry.link[0].href.split('/')[-1]
+#        if event_id == self.event_id:
+#          event = entry.to_dict()
+#          event['id'] = self.event_id
+#
+#    self.entity_collection = event
 
   def get(self):
     return self.entity_collection
