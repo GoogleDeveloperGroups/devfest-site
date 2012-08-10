3a94a677f672da531858957ae2902803cb3dfd6d
Martin Thielecke <martin@versionzwei.com>
2012-08-10 20:41:20 +0000

    - added start and end fields to form

--- devfest-site/lib/forms.py
+++ devfest-site/lib/forms.py
@@ -1,4 +1,4 @@
-from wtforms import Form, TextField, SelectField, SelectMultipleField, widgets, validators
+from wtforms import Form, TextField, SelectField, SelectMultipleField, DateTimeField, widgets, validators
 from wtforms.ext.appengine.db import model_form
 from lib.model import Event
 
@@ -11,3 +11,5 @@ class EventForm(Form):
   location = TextField('Location', [validators.Length(min=3), validators.Required()])
   status = SelectField('Status', choices=[('1', 'interested'), ('2', 'planned'), ('3', 'confirmed')])
   agenda = MultiCheckboxField('Agenda', choices=[('1', 'Conference'), ('2', 'Hackathon/VHackAndroid'), ('3', 'Barcamp'), ('4', 'Google Developer Live sessions')])
+  start = DateTimeField('Start', format="%Y-%m-%d %H:%M")
+  end = DateTimeField('End', format="%Y-%m-%d %H:%M")
