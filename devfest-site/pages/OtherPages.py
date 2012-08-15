6ceb189a7b77919f82c960c2fe0919aca0926389
Shrey Malhotra <shreks7@gmail.com>
2012-08-15 12:50:55 +0530

    - Other Pages (.py)

---
+++ devfest-site/pages/OtherPages.py
@@ -0,0 +1,25 @@
+from lib.view import FrontendPage
+from google.appengine.api import users
+
+class AboutPage(FrontendPage):
+    def show(self):
+        self.values['current_navigation'] = 'about'
+        user = users.get_current_user()
+        self.template="about"
+        
+class ReportBugPage(FrontendPage):
+    def show(self):
+        user = users.get_current_user()
+        self.template="report"   
+        
+class BrandingPage(FrontendPage):
+    def show(self):
+        user = users.get_current_user()
+        self.values['current_navigation'] = 'branding'
+        self.template="branding"
+
+class FaqPage(FrontendPage):
+    def show(self):
+        user = users.get_current_user()
+        self.values['current_navigation'] = 'faq'
+        self.template="eventfaq"   
