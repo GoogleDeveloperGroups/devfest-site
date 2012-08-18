80f6fd74992c338ec662300a2184868f11924f88
Martin Thielecke <martin@versionzwei.com>
2012-08-18 12:20:54 +0000

    - removed geocode from page

--- devfest-site/pages/EventPages.py
+++ devfest-site/pages/EventPages.py
@@ -19,15 +19,14 @@ class EventSchedulePage(FrontendPage):
 
 class EventCreatePage(FrontendPage):
   def show(self):
+    self.template = 'event_create'
     self.values['current_navigation'] = 'events'
     user = users.get_current_user()
-    if not user:
-      return self.redirect(users.create_login_url("/event/create"))
-
     form = EventForm()
     self.values['form'] = form
     self.values['form_url'] = blobstore.create_upload_url('/event/upload')
-    self.template = 'event_create'
+    if not user:
+      return self.redirect(users.create_login_url("/event/create"))
 
 class EventUploadPage(UploadPage):
   def show_post(self):
@@ -44,12 +43,8 @@ class EventUploadPage(UploadPage):
       event = Event()
       event.gplus_event_url = self.request.get('gplus_event_url')
       event.location = self.request.get('location')
-      lat, long, city, country = self.get_geolocation(self.request.get('location'))
 
       event.user = [user]
-      event.city = city
-      event.country = country
-      event.geo_location = db.GeoPt(lat, long)
 
       upload_files = self.get_uploads('logo')
       if len(upload_files) > 0:
@@ -67,32 +62,6 @@ class EventUploadPage(UploadPage):
       self.values['created_successful'] = True
     self.values['form'] = form
     self.template = 'event_create'
-
-  def get_geolocation(self, location):
-    url = u"http://maps.google.com/maps/api/geocode/json?address=%s&sensor=false" % (urllib.quote(location.encode('utf-8')))
-    result = urlfetch.fetch(url)
-    lat = 0.0
-    long = 0.0
-    city = ''
-    country = ''
-
-    if result.status_code == 200:
-      data = json.loads(result.content)
-      if 'results' in data and len(data['results']) > 0:
-        event_location = data['results'][0]
-        if 'geometry' in event_location and 'location' in event_location['geometry']:
-          lat = event_location['geometry']['location']['lat']
-          long = event_location['geometry']['location']['lng']
-
-        if 'address_components' in event_location:
-          for component in event_location['address_components']:
-            print component
-            if "locality" in component['types']:
-              city = component['long_name']
-            if "country" in component['types']:
-              country = component['long_name']
-
-    return (lat, long, city, country)
 
 class EventPage(FrontendPage):
   def show(self, *paths):
