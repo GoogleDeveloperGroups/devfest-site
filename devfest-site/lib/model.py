ed505c5bf775cffdc181eff281fb22739d30aca5
Martin Thielecke <martin@versionzwei.com>
2012-08-18 12:20:01 +0000

    - the model now knows the geocode stuff

--- devfest-site/lib/model.py
+++ devfest-site/lib/model.py
@@ -1,5 +1,8 @@
 from google.appengine.ext import db
 from google.appengine.api import users
+from google.appengine.api import urlfetch
+import urllib
+import json
 
 class Event(db.Model):
   user = db.ListProperty(users.User)
@@ -19,6 +22,37 @@ class Event(db.Model):
   kind_of_support = db.TextProperty()
   expected_participants = db.StringProperty()
   subdomain = db.StringProperty()
+  approved = db.BooleanProperty(default = False)
+
+  def set_geolocation(self):
+    location = self.location
+    url = u"http://maps.google.com/maps/api/geocode/json?address=%s&sensor=false" % (urllib.quote(location.encode('utf-8')))
+    result = urlfetch.fetch(url)
+    
+    lat = 0.0
+    long = 0.0
+    city = ''
+    country = ''
+
+    if result.status_code == 200:
+      data = json.loads(result.content)
+      if 'results' in data and len(data['results']) > 0:
+        event_location = data['results'][0]
+        if 'geometry' in event_location and 'location' in event_location['geometry']:
+          lat = event_location['geometry']['location']['lat']
+          long = event_location['geometry']['location']['lng']
+          self.geo_location = db.GeoPt(lat, long)
+
+        if 'address_components' in event_location:
+          for component in event_location['address_components']:
+            if "locality" in component['types']:
+              self.city = component['long_name']
+            if "country" in component['types']:
+              self.country = component['long_name']
+
+  def put(self, **kwargs):
+    self.set_geolocation()
+    return super(Event, self).put(**kwargs)
     
 class UserGroup(db.Model):
   gplus_id = db.StringProperty(required=True)
