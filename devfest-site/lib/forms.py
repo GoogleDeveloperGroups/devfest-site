a7533b1be5d977772e67a7d8c2e4415b152f1799
Helmuth Breitenfellner <helmuth@breitenfellner.at>
2012-08-24 23:48:23 +0200

    Attempt to beautify the code
    I know beauty is a quite personal impression, but with the old
    formatting style I did not really know how to read.

--- devfest-site/lib/forms.py
+++ devfest-site/lib/forms.py
@@ -1,38 +1,80 @@
-from wtforms import Form, TextField, TextAreaField, SelectField, SelectMultipleField, DateTimeField, FileField, widgets, validators, IntegerField, DateField
+from wtforms import (Form, TextField, TextAreaField, SelectField,
+     SelectMultipleField, DateTimeField, FileField, widgets,
+     validators, IntegerField)
 from wtforms.ext.appengine.db import model_form
 from lib.model import Event
 
+# This file contains all the forms used at the backend (besides the
+# admin-related forms)
+
+# a multi-select list composed out of many checkboxes
 class MultiCheckboxField(SelectMultipleField):
   widget = widgets.ListWidget(prefix_label=False)
   option_widget = widgets.CheckboxInput()
 
+
 class EventForm(Form):
-  gplus_event_url = TextField('Google+ Event URL', [validators.URL(), validators.Required()])
-  external_url = TextField('External URL (in an iframe or linked to)', [validators.Optional(),validators.URL()])
-  external_width = IntegerField('Width of the iframe (if embedded)', [validators.Optional(),validators.NumberRange(min=0,max=700)])
-  external_height = IntegerField('Height of the iframe (if embedded)', [validators.Optional(),validators.NumberRange(min=0,max=500)])
-  location = TextField('Location', [validators.Length(min=3), validators.Required()])
-  status = SelectField('Status', choices=[('1', 'interested'), ('2', 'planned'), ('3', 'confirmed')])
-  logo = FileField('Logo')
-  agenda = MultiCheckboxField('Agenda', choices=[('1', '<img src="\images/icons/conf-icon.png"> Conference'), ('2', '<img src="\images/icons/hack-icon.png"> Hackathon/VHackAndroid'), ('3', '<img src="\images/icons/barcamp-icon.png"> Barcamp'), ('4', '<img src="\images/icons/gdl-icon.png"> Google Developer Live sessions'),('5', '<img src="\images/icons/others-icon.png"> Others')])
-  start = DateTimeField('Start', format="%Y-%m-%d %H:%M")
-  end = DateTimeField('End', format="%Y-%m-%d %H:%M")
-  gdg_chapters = TextField('GDG Chapters', [validators.Required()], description='Comma seperated list')
-  technologies = MultiCheckboxField('What products, technologies you propose to cover in the event', choices=[('Android', 'Android'), ('Chrome', 'Chrome'), ('Google+', 'Google+'), ('App Engine', 'App Engine'), ('Games', 'Games'), ('Google Maps', 'Google Maps'), ('Google Apps', 'Google Apps'), ('Google TV', 'Google TV'), ('Commerce', 'Commerce'), ('Youtube', 'Youtube'), ('Other', 'Other')])
-  kind_of_support = TextAreaField('What kind of support you expect for this event?', [validators.Required()])
-  subdomain = TextField('Preferred subdomain for the event website')
+  gplus_event_url = TextField('Google+ Event URL',
+        [validators.URL(), validators.Required()])
+  external_url    = TextField('External URL (in an iframe or linked to)',
+        [validators.Optional(),validators.URL()])
+  external_width  = IntegerField('Width of the iframe (if embedded)',
+        [validators.Optional(),validators.NumberRange(min=0,max=700)])
+  external_height = IntegerField('Height of the iframe (if embedded)',
+        [validators.Optional(),validators.NumberRange(min=0,max=500)])
+  location        = TextField('Location',
+        [validators.Length(min=3), validators.Required()])
+  status          = SelectField('Status',
+        choices=[('1', 'interested'), ('2', 'planned'), ('3', 'confirmed')])
+  logo            = FileField('Logo')
+  agenda          = MultiCheckboxField('Agenda',
+        choices=[
+           ('1', '<img src="/images/icons/conf-icon.png"> Conference'),
+           ('2', '<img src="/images/icons/hack-icon.png"> '
+                           'Hackathon/VHackAndroid'),
+           ('3', '<img src="/images/icons/barcamp-icon.png"> Barcamp'),
+           ('4', '<img src="/images/icons/gdl-icon.png"> '
+                           'Google Developer Live sessions'),
+           ('5', '<img src="/images/icons/others-icon.png"> Others')])
+  start           = DateTimeField('Start', format="%Y-%m-%d %H:%M")
+  end             = DateTimeField('End', format="%Y-%m-%d %H:%M")
+  gdg_chapters    = TextField('GDG Chapters',
+        [validators.Required()], description='Comma separated list')
+  technologies    = MultiCheckboxField(
+         'What products, technologies you propose to cover in the event',
+        choices=[('Android', 'Android'),
+                 ('Chrome', 'Chrome'),
+                 ('Google+', 'Google+'),
+                 ('App Engine', 'App Engine'),
+                 ('Games', 'Games'),
+                 ('Google Maps', 'Google Maps'),
+                 ('Google Apps', 'Google Apps'),
+                 ('Google TV', 'Google TV'),
+                 ('Commerce', 'Commerce'),
+                 ('Youtube', 'Youtube'),
+                 ('Other', 'Other')])
+  kind_of_support = TextAreaField(
+         'What kind of support you expect for this event?',
+        [validators.Required()])
+  subdomain       = TextField('Preferred subdomain for the event website')
+
 
 class ContactForm(Form):
-  name = TextField('Your Name', [validators.Required()])
-  email = TextField('Email Address', [validators.Email(), validators.Required()])
-  organizer = SelectField('Are you GDG organizer?', choices=[('0', 'No'), ('1', 'Yes')])
-  gdg_chapter = TextField('Your GDG Chapter')
-  subject = TextField('Subject', [validators.Required()])
-  message = TextAreaField('Message', [validators.Required()])
+  name            = TextField('Your Name', [validators.Required()])
+  email           = TextField('Email Address',
+        [validators.Email(), validators.Required()])
+  organizer       = SelectField('Are you GDG organizer?',
+        choices=[('0', 'No'), ('1', 'Yes')])
+  gdg_chapter     = TextField('Your GDG Chapter')
+  subject         = TextField('Subject', [validators.Required()])
+  message         = TextAreaField('Message', [validators.Required()])
+
 
 class SponsorForm(Form):
-  name = TextField('Name of Sponsor', [validators.Required()])
-  gplus_id = IntegerField('G+ ID (21 numbers)', [validators.number_range(100000000000000000000, 999999999999999999999, "21 digits required")])
-  description = TextField("Company Description", [validators.length(20, 250, "Text length must be between 20 and 250 characters")])
-  logo = FileField('Sponsor''s Logo')
+  name            = TextField('Name of Sponsor', [validators.Required()])
+  gplus_id        = IntegerField('Google+ ID (21 digits)',
+        [validators.number_range(10 ** 20, 10 ** 21 - 1, "21 digits required")])
+  description     = TextAreaField("Company Description",
+        [validators.length(20, 250)])
+  logo            = FileField('Sponsor\'s Logo')
 
