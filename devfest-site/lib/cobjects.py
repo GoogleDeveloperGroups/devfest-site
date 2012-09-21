c7c0394163040880e1f5d1b3df509a2d2dd311e9
friedger <friedger@gmail.com>
2012-09-21 11:35:42 +0200

    show all VHackAndroid events

--- devfest-site/lib/cobjects.py
+++ devfest-site/lib/cobjects.py
@@ -190,7 +190,7 @@ class CAdminEventList(CEventList):
 class CVHAEventList(CEventList):
   def load_from_db(self):
     self.entity_collection = {}
-    events = Event.all().filter('approved =', True).filter('is_vhackandroid =', True).filter('end >=', datetime.datetime.now())
+    events = Event.all().filter('approved =', True).filter('is_vhackandroid =', True)
     event_list = {}
     for event in events:
       if event_list.has_key(event.country) is False:
